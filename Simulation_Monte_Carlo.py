e66dc4d8cd038888496260f89b76dd162f8aab12
Franciszek Kalembkiewicz <f.kalembkiewicz@wp.pl>
2025-11-18 15:44:08 +0100

    Working on monte carlo sim. added extra columns and prediction based on probablitis

--- Simulation_Monte_Carlo.py
+++ Simulation_Monte_Carlo.py
@@ -1,35 +1,51 @@
 import pandas as pd
 import random
+from Matches.PremierLeague2025_26 import matches_PremierLeague, teams_PremierLeague, league_name_PremierLeague
+
 
 def simulation(league_name, teams_df, match_days):
     df_matches = pd.read_excel(rf"Sezon\{league_name}.xlsx")
-    df_matches.insert("HomeSimulationPoints", pd.NA)
-    df_matches.insert("AwaySimulationPoints", pd.NA)
+    df_matches['HomeSimulationPoints'] = pd.NA
+    df_matches['AwaySimulationPoints'] = pd.NA
 
-    values = ["HomeWin", "Draw", "AwayWin"]
-    probabilities = [0.42, 0.26, 0.32]
-    result = random.choices(values, weights=probabilities, k=1)[0]
+    for i in range(df_matches.shape[0]):
+        values = ["HomeWin", "Draw", "AwayWin"]
+        probabilities = [
+            df_matches.at[i, "HomeWinProb"],
+            df_matches.at[i, "DrawProb"],
+            df_matches.at[i, "AwayWinProb"]
+        ]
+        result = random.choices(values, weights=probabilities, k=1)[0]
 
+        if result == "HomeWin":
+            df_matches.at[i, "HomeSimulationPoints"] = 3
+            df_matches.at[i, "AwaySimulationPoints"] = 0
+        elif result == "AwayWin":
+            df_matches.at[i, "HomeSimulationPoints"] = 0
+            df_matches.at[i, "AwaySimulationPoints"] = 3
+        elif result == "Draw":
+            df_matches.at[i, "HomeSimulationPoints"] = 1
+            df_matches.at[i, "AwaySimulationPoints"] = 1
 
-    clubs = teams_df["Club"].tolist()
-    prediction_data = {}
-    for club in clubs:
-        prediction_data[club] = []
-        points = 0
-        for i in range(0, df_matches.shape[0]):
-            if df_matches.at[i, 'Home'] == club:
-                if df_matches.at[i, 'MatchWeek'] in match_days:
-                    points += df_matches.at[i, 'HomePoints']
-                else:
-                    points += df_matches.at[i, 'HomeExp']
-            if df_matches.at[i, 'Away'] == club:
-                if df_matches.at[i, 'MatchWeek'] in match_days:
-                    points += df_matches.at[i, 'AwayPoints']
-                else:
-                    points += df_matches.at[i, 'AwayExp']
-        prediction_data[club].append(points)
+        clubs = teams_df["Club"].tolist()
+        prediction_data = {}
+        for club in clubs:
+            prediction_data[club] = []
+            points = 0
+            for i in range(0, df_matches.shape[0]):
+                if df_matches.at[i, 'Home'] == club:
+                    if df_matches.at[i, 'MatchWeek'] in match_days:
+                        points += df_matches.at[i, 'HomePoints']
+                    else:
+                        points += df_matches.at[i, 'HomeExp']
+                if df_matches.at[i, 'Away'] == club:
+                    if df_matches.at[i, 'MatchWeek'] in match_days:
+                        points += df_matches.at[i, 'AwayPoints']
+                    else:
+                        points += df_matches.at[i, 'AwayExp']
+            prediction_data[club].append(points)
 
-    return prediction_data
+        return prediction_data
 
 # def table_xlsx(league_name, teams_df, matchday):
 #     clubs = teams_df["Club"].tolist()
