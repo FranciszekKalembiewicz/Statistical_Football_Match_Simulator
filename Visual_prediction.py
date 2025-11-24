df4d87f4b83c5569208c2f5a932a18747307689a
Franciszek Kalembkiewicz <f.kalembkiewicz@wp.pl>
2025-11-24 02:28:52 +0100

    Did some prediction

---
+++ Visual_prediction.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Pobranie dzisiejszej daty (rok, miesiąc, dzień)
+dzisiaj = datetime.date.today()
+
+# Wyświetlenie wyniku
+print(dzisiaj)
+print(f"Typ obiektu: {type(dzisiaj)}")
